b2c738a2a7ca4620de61f236bfa20c043081cee7
Amy Smyth <amy@anns.co.uk>
2026-08-04 19:31:17 +0100

    Raise dashboard labels within rooms

--- components/EstateDashboard.tsx
+++ components/EstateDashboard.tsx
@@ -56,7 +56,7 @@ function EstateHotspotMarker({
 }) {
   const labelPositionClass = labelPosition === "right"
     ? "left-[calc(100%+7px)] top-1/2 -translate-y-1/2"
-    : "left-1/2 top-[calc(100%+6px)] -translate-x-1/2";
+    : "left-1/2 top-[calc(100%-4px)] -translate-x-1/2";
 
   return (
     <span
