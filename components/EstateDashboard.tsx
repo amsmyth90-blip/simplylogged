c51d78d1a5719ca28c51eff0b46edc8828b608dc
Amy Smyth <amy@anns.co.uk>
2026-08-04 20:41:23 +0100

    Fine tune garden and driveway labels

--- components/EstateDashboard.tsx
+++ components/EstateDashboard.tsx
@@ -145,8 +145,8 @@ export function EstateDashboard() {
                 ) : (
                   <EstateHotspotMarker
                     label={area.name}
-                    top={area.id === "front-gate" ? "28%" : area.id === "garden" ? "20%" : area.id === "driveway" ? "40%" : "50%"}
-                    left={area.id === "garden" ? "75%" : area.id === "driveway" ? "62%" : "50%"}
+                    top={area.id === "front-gate" ? "28%" : area.id === "garden" ? "20%" : area.id === "driveway" ? "48%" : "50%"}
+                    left={area.id === "garden" ? "65%" : area.id === "driveway" ? "62%" : "50%"}
                     labelPosition={area.id === "garden" ? "right" : "below"}
                     className={area.id === "garden" ? "!top-[42%] sm:!top-[20%]" : ""}
                   />
